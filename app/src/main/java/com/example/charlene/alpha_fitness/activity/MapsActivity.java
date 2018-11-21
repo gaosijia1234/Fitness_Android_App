c7845805fe26558766029b7de31726ce529f1ed9
Sijia Gao <sijiagaowork@gmail.com>
2018-11-20 20:50:24 -0800

    test

--- app/src/main/java/com/example/charlene/alpha_fitness/activity/MapsActivity.java
+++ app/src/main/java/com/example/charlene/alpha_fitness/activity/MapsActivity.java
@@ -63,6 +63,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        //
 //        db = DatabaseHelper.getInstance(this);
 //        // ************************* unchecked *************************
 //        // mock data to checkout addWorkout in db,
